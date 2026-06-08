9ac1e9ecca0d437562cb2b210a2ebdfed9bf6374
Erik Muller <erikm@buh.org>
2026-06-08 10:53:21 +0000

    sync to page 30

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -1,24 +1,36 @@
-#!/usr/bin/env bash
+# run magic moby script for D-in-D
+/usr/local/share/docker-init.sh
 
-set +e
-
-if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
-    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
-fi
-code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
-
-# on Codespaces this will not work correctly
-if ! ${CODESPACES:-false}; then
-    # Execute command from docker cli if any.
-    if [ ${@+True} ]; then
-        exec "$@"
-    # Otherwise just enter sh or zsh.
-    else
-        if [ -f "/bin/zsh" ]; then
-            exec zsh
-        else
-            exec sh
-        fi
-    fi
+# check if ceos-lab image already present
+if [ -z "$(${CONTAINER_ENGINE} image ls | grep 'arista/ceos')" ]; then
+    docker load < ceos_lab.tar.gz
+    echo "WARNING: cEOS-lab image was successfully loaded."
 fi
 
+# start the lab
+make start
+
+##!/usr/bin/env bash
+#
+#set +e
+#
+#if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
+#    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
+#fi
+#code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
+#
+## on Codespaces this will not work correctly
+#if ! ${CODESPACES:-false}; then
+#    # Execute command from docker cli if any.
+#    if [ ${@+True} ]; then
+#        exec "$@"
+#    # Otherwise just enter sh or zsh.
+#    else
+#        if [ -f "/bin/zsh" ]; then
+#            exec zsh
+#        else
+#            exec sh
+#        fi
+#    fi
+#fi
+#
